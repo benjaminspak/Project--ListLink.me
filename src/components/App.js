62c8d10b701ef5f6d3d3112669dfa8359f59b708
Ogundiji Bolade Adio <kamponistullar@gmail.com>
2018-09-11 19:58:09 +0100

    added prop-type, and fix placeholders for components and landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import Landing from './Pages/Landing';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout>
+        <Route exact path="/" component={Landing} />
+      </Layout>
     );
   }
 }
