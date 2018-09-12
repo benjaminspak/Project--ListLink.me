b24c7202c3cf7b444ee94749e2c213f67c98d107
Ogundiji Bolade Adio <kamponistullar@gmail.com>
2018-09-12 15:32:04 +0100

    logo size fixed

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -91,7 +91,7 @@ const header = () => (
           <a href="/">
             <Logo>
               <svg
-                width="130px"
+                width="110px"
                 height="50px"
                 viewBox="0 0 137 25"
                 xmlns="http://www.w3.org/2000/svg"
